9c6fd806249a8e76a66c3b1672f1f38648bf349e
TonisDev <117898608+TonisDev@users.noreply.github.com>
2023-05-09 16:38:19 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 console.log("Hello Console");
+import "./styles.css";
+import Donkeyclip from "@donkeyclip/react";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      <Donkeyclip height={400} staging={true} id="JLifqrak73V8PtN5cJV7zn" />
+    </div>
+  );
+}
